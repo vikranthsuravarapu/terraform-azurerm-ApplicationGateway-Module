7e31b616ff961d8a87a5d6a46f4b66136b25a903
vikranth suravarapu <v-viksur@outlook.com>
2025-03-03 10:43:43 +0530

    main.tf adding provider details

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
 resource "azurerm_public_ip" "appgw_pip" {
   name                = var.public_ip_name
   location            = var.location
